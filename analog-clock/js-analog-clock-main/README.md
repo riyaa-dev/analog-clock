e7ea826a0cb521158d7362722713e20fbbeb271f
riyaa-dev <sainiriyaa44444@gmail.com>
2024-07-03 14:34:00 +0530

    first commit

---
+++ analog-clock/js-analog-clock-main/README.md
@@ -0,0 +1,50 @@
+# Analog Clock with To-Do List Application
+
+This repository contains an analog clock design with a to-do list application built using HTML, CSS, and JavaScript. The application allows users to add, delete, and update tasks, with an alarm that warns them when the task time is reached. The tasks are listed in ascending order based on the due date and time.
+
+# Features
+
+    Add, delete, and update tasks
+    Alarm to remind users when the task time is reached
+    Tasks are sorted in ascending order based on the due date and time
+    Switch between databases using query params (?db=0 for MongoDB and ?db=1 for MySQL)
+
+# Requirements
+
+    Node.js
+    MongoDB or MySQL
+
+# Installation
+
+    Clone this repository using git clone https://github.com/tugraturkseven/js-analog-clock.git
+    Run npm install to install the required packages.
+    Start the application using npm start.
+    Open the application in a web browser at http://localhost:3000/.
+
+# Switching Between Databases
+
+To switch between MongoDB and MySQL databases, use the query params ?db=0 for MongoDB and ?db=1 for MySQL.
+
+# APIs
+
+This application includes two APIs that are published in other repositories:
+
+    task-manager-api: This API manages tasks for the application. It is available at https://github.com/tugraturkseven/task-manager-api.
+    task-manager-api-mysql: This API manages tasks for the application using a MySQL database. It is available at https://github.com/tugraturkseven/task-manager-api-mysql.
+
+# Contributing
+
+Contributions are welcome! Please feel free to submit a pull request or open an issue if you find any bugs or have any suggestions for improvements.
+
+# Contact
+
+If you have any questions or comments about this project, please feel free to contact me at contact@tugraturkseven.com.
+
+# How it looks.
+
+![gif](https://user-images.githubusercontent.com/28603785/219970057-67631900-e18d-42a3-8105-ffb87dfb32c1.gif)
+
+# Youtube Video for implementation.
+https://youtu.be/rsLiimCQuC8
+![image](https://user-images.githubusercontent.com/28603785/220934367-4c26233b-3478-4fe5-9093-90f9966c4a3e.png)
+
---
+++ analog-clock/js-analog-clock-main/app.js
@@ -0,0 +1,269 @@
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+const taskList = document.querySelector('.tasklist');
+const addBtn = document.getElementById('add');
+const editBtn = document.getElementById('editBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+const taskInput = document.getElementById('taskInput');
+const timeInput = document.getElementById('timeInput');
+const alertAudio = new Audio('assets/alert.mp3.mp3');
+const urlParams = new URLSearchParams(location.search);
+var taskArr = [];
+var dbNumber = "db0";
+
+if(urlParams.get('db')==0){
+    dbNumber = "db0";
+}else if(urlParams.get('db')==1){
+    dbNumber = "db1";
+}else if(urlParams.get('db')!=null){
+    console.log('No valid query param for db. Please choose 0 or 1', urlParams.get('db'));
+}
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    if (taskArr.length > 0) checkAlarm(taskArr, hour, mins, seconds);
+}
+
+setInterval(setDate, 1000)
+
+setDate();
+
+
+
+
+addTask = () => {
+    if (taskInput.value != "" && timeInput.value != "") {
+
+        const data = {
+            task: taskInput.value,
+            time: timeInput.value,
+        };
+
+        fetch(`http://localhost:8080/api/todos/${dbNumber}`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then((response) => response.json())
+            .then((sts) => {
+                console.log("Başarılı.");
+                readFromDatabase();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+
+        taskInput.value = '';
+        timeInput.value = '';
+
+
+    } else {
+        if (taskInput.value == "")
+            alert('Lütfen görev bilgisini boş bırakmayınız!');
+        else alert('Lütfen görev zamanını seçiniz!');
+    }
+}
+
+editTask = (selectedTask) => {
+
+    let selected = selectedTask.parentElement.firstChild;
+    let id = selectedTask.parentElement.id;
+    let timeEdit = selectedTask.previousSibling;
+
+
+    if (selected.readOnly == true) {
+        selected.readOnly = false;
+        timeEdit.readOnly = false;
+
+        selectedTask.innerText = '✔️';
+
+
+        selected.classList.remove('task');
+        selected.classList.add('editmode');
+        timeEdit.classList.remove('task-time');
+        timeEdit.classList.add('time-edit-mode');
+    } else {
+        selected.readOnly = true;
+        timeEdit.readOnly = true;
+
+        selected.classList.remove('editmode');
+        selected.classList.add('task');
+        timeEdit.classList.remove('time-edit-mode');
+        timeEdit.classList.add('task-time');
+
+        const data = {
+            task: selected.value,
+            time: timeEdit.value,
+        };
+
+        fetch(`http://localhost:8080/api/todos/${dbNumber}/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then((response) => response.json())
+            .then((sts) => {
+                console.log("Başarılı.");
+                readFromDatabase();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+
+        
+
+        selectedTask.innerText = '✏️';
+    }
+
+
+}
+
+createTask = (id, taskDesc, taskHour) => {
+    let liElm, inputElm, timeElm, edit, dlt;
+
+    liElm = document.createElement('li');
+
+    inputElm = document.createElement('input');
+    inputElm.value = taskDesc;
+
+    timeElm = document.createElement('input');
+    timeElm.setAttribute('type', 'time');
+    timeElm.setAttribute('readonly', 'readonly');
+    timeElm.classList.add('task-time');
+    timeElm.value = taskHour;
+
+
+    inputElm.setAttribute('readonly', 'readonly')
+    inputElm.classList.add('task');
+
+
+    edit = document.createElement('button');
+    edit.innerText = '✏️';
+    edit.setAttribute('id', 'editBtn')
+    edit.setAttribute('onclick', 'editTask(this)');
+
+
+    dlt = document.createElement('button');
+    dlt.innerText = '🗑️';
+    dlt.setAttribute('id', 'deleteBtn');
+    dlt.setAttribute('onclick', 'deleteTask(this)');
+
+    liElm.appendChild(inputElm);
+    liElm.appendChild(timeElm);
+    liElm.appendChild(edit);
+    liElm.appendChild(dlt);
+    liElm.setAttribute('id', id);
+
+    return liElm;
+}
+
+
+deleteTask = (selectedTask) => {
+    let selected = selectedTask.parentElement;
+    let id = selected.id;
+    taskList.removeChild(selected);
+
+    fetch(`http://localhost:8080/api/todos/${dbNumber}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then((response) => response.json())
+        .then((sts) => {
+            console.log("Başarılı.");
+            readFromDatabase();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    
+}
+
+checkAlarm = (tasks, hour, mins, secs) => {
+    hour = fixTime(hour);
+    mins = fixTime(mins);
+    secs = fixTime(secs);
+
+    tasks.forEach(item => {
+        let alertTime = item.firstChild.nextSibling.value + ':' + '00';
+        let currTime = hour + ':' + mins + ':' + secs;
+        if (alertTime == currTime) {
+            alertAudio.play();
+            alert(item.firstChild.value + ' görevi için alarm!');
+            item.firstChild.style.textDecoration = 'line-through';
+        }
+    })
+}
+
+fixTime = (x) => {
+    if (x < 10) {
+        x = '0' + x;
+    }
+    return x;
+}
+
+sortTasks = () => {
+    taskArr.sort((a, b) => {
+        const firstTaskTime = a.firstChild.nextSibling.value;
+        const secondTaskTime = b.firstChild.nextSibling.value;
+        const firstTaskHour = Number(firstTaskTime.slice(0, 2));
+        const firstTaskMinute = Number(firstTaskTime.slice(3));
+        const secondTaskHour = Number(secondTaskTime.slice(0, 2));
+        const secondTaskMinute = Number(secondTaskTime.slice(3));
+
+
+        if (firstTaskHour - secondTaskHour == 0) {
+            return firstTaskMinute - secondTaskMinute;
+        } else return firstTaskHour - secondTaskHour;
+    });
+
+    taskList.innerHTML = "";
+    taskArr.forEach(item => {
+        taskList.appendChild(item);
+    });
+}
+
+// Read tasks from database
+
+readFromDatabase = () => {
+    fetch(`http://localhost:8080/api/todos/${dbNumber}`)
+        .then((response) => response.json())
+        .then((sts) => {
+            taskArr = [];
+            if(dbNumber=="db0"){
+                sts.todos.forEach((item) => {
+                    const task = createTask(item._id, item.task, item.time)
+                    taskArr.push(task);
+                })
+            }else if(dbNumber=="db1"){
+                sts.forEach((item) => {
+                    const task = createTask(item.id, item.task, item.time)
+                    taskArr.push(task);
+                })
+            }
+            sortTasks();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
+
+readFromDatabase();
